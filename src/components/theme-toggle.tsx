c91eb8b24c54db13a605383f5f1a14b9ffe6dc11
elkortby <kortbyoussama@gmail.com>
2025-09-26 11:49:26 +0100

    icon

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { Sun, Moon } from "lucide-react";
+
 export const ThemeToggle = () => {
-  return null;
+  const [theme, setTheme] = useState("system");
+
+  return theme === "dark" ? <Sun className="w-6 h-6" /> : <Moon className="w-6 h-6" />;
 }
